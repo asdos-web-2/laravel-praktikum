5a2a5a42d021322748c8dc74ef7d5ae05914583e
Farhan <anak.upload90@gmail.com>
2025-03-16 23:47:39 +0800

    menambah method index show dan create ke dalam controller

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -6,5 +6,18 @@ use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
-    //
+    public function index()
+    {
+        return 'students data...';
+    }
+
+    public function show(string $id)
+    {
+        return 'Student ' . $id;
+    }
+
+    public function create()
+    {
+        return "create student data";
+    }
 }
