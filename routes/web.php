fb3b1a40f150249aa1dc9ffbb360dc1191ebb523
Farhan <anak.upload90@gmail.com>
2025-03-16 23:38:33 +0800

    buat contoh contoh routing

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,25 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Basic routing
+Route::get('/greeting', function () {
+    return 'Hello World';
+});
+
+// Redirect Routing
+Route::redirect('/redirect', '/greeting');
+
+
+// View Routing
+Route::view('/view', 'view');
+
+// Routing with parameters
+Route::get('/user/{id}', function (string $id) {
+    return 'User ' . $id;
+});
+
+// Named routes
+Route::get('/user/profile', function () {
+    return "user profile";
+})->name('profile');
