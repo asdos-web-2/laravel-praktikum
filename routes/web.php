ef7252f9904cb48ab4c7ea988be88d86f39601ae
Farhan <anak.upload90@gmail.com>
2025-03-18 22:32:51 +0800

    menyesuaikan urutan routes agar tidak bentrok

--- routes/web.php
+++ routes/web.php
@@ -16,40 +16,42 @@ Route::get('/', function () {
 // // Redirect Routing
 // Route::redirect('/redirect', '/students');
 
-// // Routing with parameters
-// Route::get('/students/{id}', function (string $id) {
-//     return 'Student ' . $id;
-// });
-
 // // Named routes
 // Route::get('/students/create', function () {
 //     return "create student data";
 // })->name('students.create');
 
+// // Routing with parameters
+// Route::get('/students/{id}', function (string $id) {
+//     return 'Student ' . $id;
+// });
+
+
 // connect to controller
 // Route::get('/students', [StudentController::class, 'index']);
 
+// Route::get('/students/create', [StudentController::class, 'create'])->name('students.create');
+
 // Route::get('/students/{id}', [StudentController::class, 'show']);
 
-// Route::get('/students/create', [StudentController::class, 'create'])->name('students.create');
 
 // group route by controller
 // Route::controller(StudentController::class)->group(function () {
 //     Route::get('/students', 'index');
-//     Route::get('/students/{id}', 'show');
 //     Route::get('/students/create', 'create');
+//     Route::get('/students/{id}', 'show');
 // });
 
 // group prefix
-Route::prefix('students')->group(function () {
-    Route::get('/', [StudentController::class, 'index']);
-    Route::get('/{id}', [StudentController::class, 'show']);
-    Route::get('/create', [StudentController::class, 'create'])->name('create');
-});
+// Route::prefix('students')->group(function () {
+//     Route::get('/', [StudentController::class, 'index']);
+//     Route::get('/create', [StudentController::class, 'create'])->name('create');
+//     Route::get('/{id}', [StudentController::class, 'show']);
+// });
 
-// group name prefixes
-Route::name('students.')->group(function () {
-    Route::get('/students', [StudentController::class, 'index']);
-    Route::get('/students/{id}', [StudentController::class, 'show']);
-    Route::get('/students/create', [StudentController::class, 'create'])->name('create');
-});
+// // group name prefixes
+// Route::name('students.')->group(function () {
+//     Route::get('/students', [StudentController::class, 'index']);
+//     Route::get('/students/create', [StudentController::class, 'create'])->name('create');
+//     Route::get('/students/{id}', [StudentController::class, 'show']);
+// });
