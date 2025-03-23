0c1ae30081bf407141a27fb7d138afe1bebb136e
Farhan <anak.upload90@gmail.com>
2025-03-23 18:44:09 +0800

    terapkan controller ke routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 
@@ -7,51 +8,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
-// // Basic routing
-// Route::get('/students', function () {
-//     return 'students data...';
-// });
-
-// // Redirect Routing
-// Route::redirect('/redirect', '/students');
-
-// // Named routes
-// Route::get('/students/create', function () {
-//     return "create student data";
-// })->name('students.create');
-
-// // Routing with parameters
-// Route::get('/students/{id}', function (string $id) {
-//     return 'Student ' . $id;
-// });
-
-
-// connect to controller
-// Route::get('/students', [StudentController::class, 'index']);
-
-// Route::get('/students/create', [StudentController::class, 'create'])->name('students.create');
-
-// Route::get('/students/{id}', [StudentController::class, 'show']);
-
-
-// group route by controller
-// Route::controller(StudentController::class)->group(function () {
-//     Route::get('/students', 'index');
-//     Route::get('/students/create', 'create');
-//     Route::get('/students/{id}', 'show');
-// });
-
-// group prefix
-// Route::prefix('students')->group(function () {
-//     Route::get('/', [StudentController::class, 'index']);
-//     Route::get('/create', [StudentController::class, 'create'])->name('create');
-//     Route::get('/{id}', [StudentController::class, 'show']);
-// });
-
-// // group name prefixes
-// Route::name('students.')->group(function () {
-//     Route::get('/students', [StudentController::class, 'index']);
-//     Route::get('/students/create', [StudentController::class, 'create'])->name('create');
-//     Route::get('/students/{id}', [StudentController::class, 'show']);
-// });
+Route::get('/dashboard', [DashboardController::class, "index"])->name('dashboard');
+Route::get('/students', [StudentController::class, 'index'])->name("students.index");
+Route::get('/students/create', [StudentController::class, 'create'])->name("students.create");;
+Route::get('/students/{id}', [StudentController::class, 'show'])->name("students.show");;
+Route::get('/students/{id}/edit', [StudentController::class, 'edit'])->name("students.edit");;
