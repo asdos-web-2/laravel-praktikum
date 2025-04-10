70db34afafd354de10b97a7859e85f1b8c0f5c8e
Farhan <anak.upload90@gmail.com>
2025-04-10 08:26:50 +0800

    perbarui routes

--- routes/web.php
+++ routes/web.php
@@ -10,10 +10,15 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', [DashboardController::class, "index"])->name('dashboard');
+
 Route::get('/students', [StudentController::class, 'index'])->name("students.index");
 Route::get('/students/create', [StudentController::class, 'create'])->name("students.create");
+Route::post('/students', [StudentController::class, 'store'])->name("students.store");
 Route::get('/students/{id}', [StudentController::class, 'show'])->name("students.show");
 Route::get('/students/{id}/edit', [StudentController::class, 'edit'])->name("students.edit");
+Route::put('/students/{id}', [StudentController::class, 'update'])->name("students.update");
+Route::delete('/students/{id}', [StudentController::class, 'destroy'])->name("students.destroy");
+
 
 Route::get("/majors", [MajorsController::class, "index"])->name("majors.index");
 Route::get("/majors/create", [MajorsController::class, "create"])->name("majors.create");
