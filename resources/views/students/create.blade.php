e6adc9e9be5292554ec3fc8fc9fd44be3ced4312
Farhan <anak.upload90@gmail.com>
2025-04-10 08:33:58 +0800

    perbarui view student agar bisa berinteraksi dengan controller

--- resources/views/students/create.blade.php
+++ resources/views/students/create.blade.php
@@ -1,6 +1,6 @@
 <x-default-layout title="Student" section_title="Add new student">
     <div class="grid grid-cols-3">
-        <form action="" method="POST"
+        <form action="{{ route("students.store") }}" method="POST"
             class="flex flex-col gap-4 px-6 py-4 bg-white border border-zinc-300 shadow col-span-3 md:col-span-2">
             @csrf
             @method("POST")
@@ -8,58 +8,85 @@
                 <div class="flex flex-col gap-2">
                     <label for="name">Name</label>
                     <input type="text" id="name" name="name" class="px-3 py-2 border border-zinc-300 bg-slate-50"
-                        placeholder="Student Name">
+                        placeholder="Student Name" value={{ old("name") }}>
+                    @error('name')
+                        <div class="text-red-500">{{ $message }}</div>
+                    @enderror
                 </div>
                 <div class="flex flex-col gap-2">
                     <label for="student_id_number">Student ID Number</label>
                     <input type="text" id="student_id_number" name="student_id_number"
-                        class="px-3 py-2 border border-zinc-300 bg-slate-50" placeholder="student ID (e.g., F55122001)">
+                        class="px-3 py-2 border border-zinc-300 bg-slate-50" placeholder="student ID (e.g., F55122001)"
+                        value={{ old("student_id_number") }}>
+                    @error('student_id_number')
+                        <div class="text-red-500">{{ $message }}</div>
+                    @enderror
                 </div>
             </div>
             <div class="grid sm:grid-cols-2 gap-4">
                 <div class="flex flex-col gap-2">
                     <label for="email">Email</label>
                     <input type="email" id="email" name="email" class="px-3 py-2 border border-zinc-300 bg-slate-50"
-                        placeholder="student@example.com">
+                        placeholder="student@example.com" value={{ old("email") }}>
+                    @error('email')
+                        <div class="text-red-500">{{ $message }}</div>
+                    @enderror
                 </div>
                 <div class="flex flex-col gap-2">
                     <label for="phone_number">Phone Number</label>
                     <input type="phone_number" id="phone_number" name="phone_number"
                         class="px-3 py-2 border border-zinc-300 bg-slate-50"
-                        placeholder="Phone number (e.g., 081234567891)">
+                        placeholder="Phone number (e.g., 081234567891)" value={{ old("phone_number") }}>
+                    @error('phone_number')
+                        <div class="text-red-500">{{ $message }}</div>
+                    @enderror
                 </div>
             </div>
             <div class="flex flex-col gap-2">
                 <label for="birth_date">Birth date</label>
                 <input type="date" id="birth_date" name="birth_date"
-                    class="px-3 py-2 border border-zinc-300 bg-slate-50">
+                    class="px-3 py-2 border border-zinc-300 bg-slate-50" value={{ old("birth_date") }}>
+                @error('birth_date')
+                    <div class="text-red-500">{{ $message }}</div>
+                @enderror
             </div>
             <div class="flex flex-col gap-2">
                 <label for="gender">Gender</label>
                 <select name="gender" id="gender" class="px-3 py-2 border border-zinc-300 appearance-none bg-slate-50">
-                    <option value="" disabled selected>Select Gender</option>
-                    <option value="male">Male</option>
-                    <option value="female">Female</option>
+                    <option value="" disabled {{ old('gender') == '' ? 'selected' : '' }}>Select Gender</option>
+                    <option value="male" {{ old('gender') == 'male' ? 'selected' : '' }}>Male</option>
+                    <option value="female" {{ old('gender') == 'female' ? 'selected' : '' }}>Female</option>
                 </select>
+                @error('gender')
+                    <div class="text-red-500">{{ $message }}</div>
+                @enderror
             </div>
             <div class="flex flex-col gap-2">
                 <label for="majors">Majors</label>
                 <select name="majors" id="majors" class="px-3 py-2 border border-zinc-300 appearance-none bg-slate-50">
                     <option value="" disabled selected>Select Majors</option>
-                    {{-- majors data render here --}}
-                    <option value="1">S1 Teknik Informatika</option>
-                    <option value="2">S1 Sistem Informasi</option>
+                    @foreach ($majors as $major)
+                        <option value={{ $major->id }} {{ old('majors') == $major->id ? 'selected' : '' }}>{{ $major->name }}
+                        </option>
+                    @endforeach
                 </select>
+                @error('majors')
+                    <div class="text-red-500">{{ $message }}</div>
+                @enderror
             </div>
             <div class="flex flex-col gap-2">
                 <label for="status">Status</label>
                 <select name="status" id="status" class="px-3 py-2 border border-zinc-300 appearance-none bg-slate-50">
                     <option value="" disabled selected>Select Student Status</option>
-                    <option value="active">Active</option>
-                    <option value="on leave">On Leave</option>
-                    <option value="graduated">Graduated</option>
-                    <option value="dropped out">Dropped Out</option>
+                    <option value="active" {{ old('status') == 'active' ? 'selected' : '' }}>Active</option>
+                    <option value="on leave" {{ old('status') == 'on leave' ? 'selected' : '' }}>On Leave</option>
+                    <option value="graduated" {{ old('status') == 'graduated' ? 'selected' : '' }}>Graduated</option>
+                    <option value="dropped out" {{ old('status') == 'dropped out' ? 'selected' : '' }}>Dropped Out
+                    </option>
                 </select>
+                @error('status')
+                    <div class="text-red-500">{{ $message }}</div>
+                @enderror
             </div>
             <div class="self-end flex gap-2">
                 <a href="{{ route("students.index") }}"
--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -1,4 +1,9 @@
 <x-default-layout title="Student" section_title="Students">
+    @if (session('success'))
+        <div class="bg-green-50 border border-green-500 text-green-500 px-3 py-2">
+            {{ session('success') }}
+        </div>
+    @endif
     <div class="flex">
         <a href="{{ route('students.create') }}"
             class="bg-green-50 text-green-500 border border-green-500 px-3 py-2 flex items-center gap-2">
@@ -21,27 +26,34 @@
             </thead>
             <tbody class="text-zinc-700 text-sm font-light">
                 {{-- render data here --}}
-                <tr class="border-b border-zinc-200 hover:bg-zinc-100">
-                    <td class="py-3 px-6 text-left">1</td>
-                    <td class="py-3 px-6 text-left">Farhan</td>
-                    <td class="py-3 px-6 text-center">F55122071</td>
-                    <td class="py-3 px-6 text-center">Male</td>
-                    <td class="py-3 px-6 text-center">S1 Teknik Informatika</td>
-                    <td class="py-3 px-6 text-center">Active</td>
-                    <td class="py-3 px-6 flex justify-center gap-1">
-                        <a href="{{ route("students.show", 1) }}"
-                            class="bg-blue-50 border border-blue-500 p-2 inline-block">
-                            <i class="ph ph-eye block text-blue-500"></i>
-                        </a>
-                        <a href="{{ route("students.edit", 1) }}"
-                            class="bg-yellow-50 border border-yellow-500 p-2 inline-block">
-                            <i class="ph ph-note-pencil block text-yellow-500"></i>
-                        </a>
-                        <button class="bg-red-50 border border-red-500 p-2">
-                            <i class="ph ph-trash-simple block text-red-500"></i>
-                        </button>
-                    </td>
-                </tr>
+
+                @foreach ($students as $student)
+                    <tr class="border-b border-zinc-200 hover:bg-zinc-100">
+                        <td class="py-3 px-6 text-left">{{ $loop->iteration }}</td>
+                        <td class="py-3 px-6 text-left">{{ $student->name }}</td>
+                        <td class="py-3 px-6 text-center">{{ $student->student_id_number }}</td>
+                        <td class="py-3 px-6 text-center">{{ $student->gender }}</td>
+                        <td class="py-3 px-6 text-center">{{ $student->majors->name }}</td>
+                        <td class="py-3 px-6 text-center">{{ $student->status }}</td>
+                        <td class="py-3 px-6 flex justify-center gap-1">
+                            <a href="{{ route("students.show", $student->id) }}"
+                                class="bg-blue-50 border border-blue-500 p-2 inline-block">
+                                <i class="ph ph-eye block text-blue-500"></i>
+                            </a>
+                            <a href="{{ route("students.edit", $student->id) }}"
+                                class="bg-yellow-50 border border-yellow-500 p-2 inline-block">
+                                <i class="ph ph-note-pencil block text-yellow-500"></i>
+                            </a>
+                            <form onsubmit="return confirm('Are you sure?')" method="POST" action="{{ route("students.destroy", $student->id) }}">
+                                @method("DELETE")
+                                @csrf
+                                <button type="submit" class="bg-red-50 border border-red-500 p-2">
+                                    <i class="ph ph-trash-simple block text-red-500"></i>
+                                </button>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
             </tbody>
         </table>
     </div>
